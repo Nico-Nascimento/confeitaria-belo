2caa7c325cae70434149e405f4e1748958bf3e0c
Nico-Nascimento <nico.nascimento.97@gmail.com>
2025-10-13 09:40:44 -0300

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
-import Head from "next/head";
+
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -10,15 +10,15 @@ const poppins = Poppins({
 export const metadata = {
   title: "Belô Doces e Sabores",
   description: "Cardápio digital de bolos e doces",
+  icons: {
+    icon: "/favicon.ico",      
+    apple: "/apple-touch-icon.png", 
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <Head>
-        <title>Cardápio da Belô</title>
-        <link rel="icon" type="image/png" href="logo.png" />
-      </Head>
       <body>{children}</body>
     </html>
   );
